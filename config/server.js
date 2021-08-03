49fe91973687ce54751833141e62a912b03f8bfe
Kol124 <65050225+Kol124@users.noreply.github.com>
2021-08-03 17:59:14 +0100

    First Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '580a8f486ed42611394f77c6d398106e'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '515581e1-5720-4db7-b345-1aa8dcbdc6a5'
+};
